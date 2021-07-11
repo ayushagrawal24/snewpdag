8e0c29820701ccf81b0ee3c7c40eb76ec208d370
ayushagrawal24 <ayush2001@gmail.com>
2021-07-11 03:55:59 +0100

    Added test containing a sharp cutoff

--- snewpdag/tests/test_sharp_drop.py
+++ snewpdag/tests/test_sharp_drop.py
@@ -2,15 +2,33 @@
 import unittest
 import numpy as np
 import json
+import logging
 from snewpdag.plugins.Features import SharpDropoff as sd
 
 class TestSharpDropoff(unittest.TestCase):
-  def test_alert(self):
+  def setUp(self):
     with open('snewpdag/data/test-flux-input.json', 'r') as f:
       #Only testing with KM3Net data for now
       self.data = json.load(f)[1]
       
-    sd_plugin = sd.SharpDropoff('t_low', 't_bins', name=self.data["name"])
-    sd_plugin.alert(self.data)
+    self.sd_plugin = sd.SharpDropoff('t_low', 't_bins', name=self.data["name"])
+    
+  def test_alert_false(self):
+    self.sd_plugin.alert(self.data)
     
     self.assertFalse(self.data['has_drop'])
+    logging.info('test_alert_false success: {} \n\n'.format(self.data['has_drop'] == False))
+  
+    
+  def test_alert_true(self):
+    vals = self.data['t_bins']    
+    vals[2260] = 1800
+    vals[2261:6000] = np.random.normal(1555, 40, 6000 - 2261)
+    
+    
+    self.data['t_bins'] = vals
+    
+    self.sd_plugin.alert(self.data)
+    logging.info('test_alert_true success: {} \n\n'.format(self.data['has_drop'] == True))
+    self.assertTrue(self.data['has_drop'])
+    
